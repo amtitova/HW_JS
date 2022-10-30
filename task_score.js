681667c8ddc5c54d3bf9e19648c7c918a868439c
Anna Titova <amtitova@gmail.com>
2022-10-30 14:26:20 +0100

    Home work 5.4 complete

--- task_score.js
+++ task_score.js
@@ -3,10 +3,11 @@ const studentSurname = "Titova";
 const introScore = 234;
 const gitScore = 456;
 const jsScore = 567;
+const firstSymbol = studentName.slice(0, 1);
 
 const average = (introScore + gitScore + jsScore) / 3;
 
 console.log(
-  `Student ${studentName} ${studentSurname} has next scores: intro - ${introScore}, git - ${gitScore}, js - ${jsScore}`
+  `Student ${firstSymbol}. ${studentSurname} has next scores: intro - ${introScore}, git - ${gitScore}, js - ${jsScore}`
 );
 console.log(`Average score for modules is: ${average}`);
